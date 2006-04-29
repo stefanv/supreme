35208a8da3d00f99e9a03adaa049144b502306d6
Stefan van der Walt <stefan@voyager>
2006-04-29 02:41:21 +0200

    Change shape of internal representation, so that stacking takes place in the last dimension.

--- supreme/geometry/grid.py
+++ supreme/geometry/grid.py
@@ -11,8 +11,7 @@ class Grid(object):
         Create a regular grid given rows and columns.
         
         The grid is of the specified size, with each element describing the
-        (x,y,z) co-ordinate of the top right-hand corner of a pixel (note that
-        x and y here are the columns and the rows respectively).
+        (x,y,z) co-ordinate of the top right-hand corner of a pixel.
 
         The elements of the grid can be accessed using record notation, i.e.
 
@@ -20,41 +19,31 @@ class Grid(object):
 
         A regular grid allows for transformations, as long as the
         neighbours of each coordinate stay the same.
+
+        TODO: ^ Better description of the above.
         """
 
         self._grid = N.empty(shape=(rows,columns),
                              dtype=[('x',SC.ftype),
                                     ('y',SC.ftype),
                                     ('z',SC.ftype)])
-        cgrid = N.empty((3, rows, columns), dtype=SC.ftype)
-        cgrid[:2, :, :] = N.float32(N.mgrid[:columns, :rows].swapaxes(1,2))
-        cgrid[2,:,:].fill(1)
 
-        # c = cgrid generates a mesh with
+        # A meshgrid X of shape (2,columns,rows) are generated. Here,
+        # X[0,...]^T represent the y co-ordinates, and
+        # X[1,...]^T represent the x co-ordinates.
         #
-        # c[0,:,:] = y values
-        # c[1,:,:] = x values
+        # We need to reshape these fields to have a new matrix Y for
+        # which
+        # Y[...,0] are the x co-ordinates and
+        # Y[...,1] are the y co-ordinates.
         #
-        # axes (0,1,2)
-        #       ^ ^ ^
-        #       | | `- column-wise position in grid
-        #       | `--- row-wise position in grid
-        #       `----- stacked dimension (x, y values)
-        #
-        # If we flatten this matrix, iteration will be done along
-        # dimension 2, then 1 then 0.  We want iteration over the stacked
-        # dimension, therefore we reverse axes by doing
-        #
-        # c = c.swapaxes(0,2)
-        #
-        # Since cgrid produces (row,column) and not (x,y) we need to do
-        # another axes swop:
-        #
-        # c = c.swapaxes(0,1)
-        #
-        # So the final axes are (1,2,0)
-   
-        self._grid.view((SC.ftype, 3))[:] = cgrid.swapaxes(0,2).swapaxes(0,1)[:]
+        # This is achieved by doing X.swapaxes(0,2)
+        cgrid = N.empty((rows,columns,3), dtype=SC.ftype)        
+        cgrid[...,:2] = N.mgrid[:columns,:rows].swapaxes(0,2).astype(SC.ftype)
+        cgrid[...,2].fill(1)
+
+        # We can't just use _grid[:], since those are not of type SC.ftype
+        self._grid.view((SC.ftype, 3))[:] = cgrid[:]
 
     def __getitem__(self, *args, **kwargs):
         return self._grid.__getitem__(*args, **kwargs)
