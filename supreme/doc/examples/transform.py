8803bdd38dfd7cf0db5eb50beb41d3a4ce057fbe
Stefan van der Walt <stefan@voyager>
2006-06-06 14:13:43 +0200

    Show alpha layer more clearly. Add title labels.

--- supreme/doc/examples/transform.py
+++ supreme/doc/examples/transform.py
@@ -10,8 +10,11 @@ set_local_path('../../..')
 import supreme
 restore_path()
 
-
-for im in ['misc/nova_grey.png',  'misc/nova.png', 'misc/nova_alpha.png',]:
+images = ['misc/nova_grey.png', 'misc/nova.png', 'misc/nova_alpha.png']
+im_descr = {'misc/nova_grey.png': 'Grey-level input',
+           'misc/nova.png': 'Colour input',
+	   'misc/nova_alpha.png': 'Alpha layered input'} 
+for im in images:
     img1 = supreme.imread(os.path.join(supreme.config.data_path,im))
     print "Input image shape: ", img1.shape
                                    
@@ -22,11 +25,20 @@ for im in ['misc/nova_grey.png',  'misc/nova.png', 'misc/nova_alpha.png',]:
     toc = timeit.time.time()
     print "Transform completed in %.2f seconds." % (toc-tic)
 
+    if img2.ndim == 3 and img2.shape[2] == 4:
+	img1[...,1] = img1[...,1] & img1[...,3]
+	img1 = img1[...,0:3]
+	
+	img2[...,1] = img2[...,1] & img2[...,3]
+	img2 = img2[...,0:3]
+	
     P.figure()
     P.subplot(121)
+    P.title(im_descr[im])    
     P.imshow(img1,cmap=P.cm.gray)
     P.subplot(122)
     P.imshow(img2,origin='upper',cmap=P.cm.gray)
+    P.title('Log polar transform')
     P.xlabel('Log distance')
     P.ylabel(r'Angle')
     P.show()
