c61ef1fad586888ba1bad4707b150b2c2cfde8fc
Stefan van der Walt <stefan@voyager>
2006-06-06 13:54:58 +0200

    Show grey-scale, colour and alpha images in example.

--- supreme/doc/examples/transform.py
+++ supreme/doc/examples/transform.py
@@ -10,23 +10,24 @@ set_local_path('../../..')
 import supreme
 restore_path()
 
-img1 = supreme.imread(os.path.join(supreme.config.data_path,
-                                   'misc/nova_alpha.png'))
-print "Input image shape: ", img1.shape
-                                   
-import timeit
-tic = timeit.time.time()
-img2 = supreme.transform.logpolar(img1)
-print "Output image shape: ", img2.shape
-toc = timeit.time.time()
-print "Transform completed in %.2f seconds." % (toc-tic)
 
-P.figure()
-P.subplot(121)
-P.imshow(img1)
-P.subplot(122)
-P.imshow(img2,origin='upper')
-P.xlabel('Log distance')
-P.ylabel(r'Angle')
-P.show()
-P.close()
+for im in ['misc/nova_grey.png',  'misc/nova.png', 'misc/nova_alpha.png',]:
+    img1 = supreme.imread(os.path.join(supreme.config.data_path,im))
+    print "Input image shape: ", img1.shape
+                                   
+    import timeit
+    tic = timeit.time.time()
+    img2 = supreme.transform.logpolar(img1)
+    print "Output image shape: ", img2.shape
+    toc = timeit.time.time()
+    print "Transform completed in %.2f seconds." % (toc-tic)
+
+    P.figure()
+    P.subplot(121)
+    P.imshow(img1,cmap=P.cm.gray)
+    P.subplot(122)
+    P.imshow(img2,origin='upper',cmap=P.cm.gray)
+    P.xlabel('Log distance')
+    P.ylabel(r'Angle')
+    P.show()
+    P.close()
