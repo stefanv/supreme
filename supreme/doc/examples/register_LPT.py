2d5df91efeefc182de4724f99985b80e17fd7bee
Stefan van der Walt <stefan@voyager>
2006-06-30 15:34:53 +0200

    Use correlation instead of convolution.

--- supreme/doc/examples/register_LPT.py
+++ supreme/doc/examples/register_LPT.py
@@ -4,18 +4,17 @@ from numpy.testing import set_local_path, restore_path
 import numpy as N
 import pylab as P
 import scipy as S
-S.pkgload('signal')
 
 import os.path
 
 set_local_path('../../..')
 import supreme
-from supreme.config import data_path
+from supreme.config import data_path,ftype
 from supreme import transform
 restore_path()
 
 img1 = supreme.imread(os.path.join(data_path,'NASA/hubble_crop.jpg'),flatten=True)
-img2 = supreme.imread(os.path.join(data_path,'NASA/hubble_crop_rot25.jpg'),flatten=True)
+img2 = supreme.imread(os.path.join(data_path,'NASA/hubble_crop_rot25_centre.jpg'),flatten=True)
 
 img1_tf = transform.logpolar(img1)
 img2_tf = transform.logpolar(img2)
@@ -29,14 +28,15 @@ P.imshow(img1_tf,cmap=P.cm.gray)
 P.subplot(224)
 P.imshow(img2_tf,cmap=P.cm.gray)
 
-cv = S.signal.fftconvolve(img1,img1)
-#cv = cv/cv.max()
+cv = supreme.register.fft_correlate(img1_tf,img2_tf)
 P.figure()
 P.imshow(cv)
 P.colorbar()
 
 print "Dims:", cv.shape
-print "Max idx:", N.unravel_index(cv.argmax(),cv.shape)
+mi = N.unravel_index(cv.argmax(),cv.shape)
+print "Max idx:", mi
+print "Diff:", N.array(mi,ftype) - (N.array(cv.shape)-1)/2.
 
 P.show()
 P.close()
