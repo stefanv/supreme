180ba8092ea3256cbf6bc5654edd7e05e776e2b0
Stefan van der Walt <stefan@voyager>
2006-10-03 02:10:57 +0200

    Pick up C-files in ext directory automatically.  Add scons build file.

--- supreme/ext/setup.py
+++ supreme/ext/setup.py
@@ -1,9 +1,10 @@
-from os.path import join
+from os.path import join, dirname
+from glob import glob
 
 def configuration(parent_package='', top_path=None, package_name='ext'):
     from numpy.distutils.misc_util import Configuration
     config = Configuration(package_name,parent_package,top_path)
-    config.add_extension('libsupreme_', sources=['polygon.c','filter.c'])
+    config.add_extension('libsupreme_', sources=glob(join(dirname(__file__),'*.c')))
     config.add_data_dir('tests')
     return config
 
