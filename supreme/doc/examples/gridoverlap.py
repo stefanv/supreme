99f43cc2a3878c46bb3fd235ec91ca19def73303
Stefan van der Walt <stefan@voyager>
2006-10-08 23:45:48 +0200

    Use new polygon code in grid overlap demo.

--- supreme/doc/examples/gridoverlap.py
+++ supreme/doc/examples/gridoverlap.py
@@ -1,74 +1,75 @@
-# Create two 10x10 grids, one rotated and translated
-# Calculate and display the overlap
-# Each block in the grid is a polygon
+"""
+Create two 10x10 grids, one rotated and translated.
+Calculate and display the overlap of a single block.
+"""
 
-from numpy import array, transpose, dot
-from pylab import *
-from math import pi, cos, sin
-from Polygon import Polygon, pointList
+from numpy.testing import set_local_path, restore_path
+import numpy as N
+import pylab as P
 from matplotlib import patches
+
+set_local_path('../../..')
+from supreme.geometry import Polygon
+from supreme import ext
+restore_path()
+
 
 def plotgrid(grid, mode='k-'):
     for b in grid:
-        x = [c[0] for c in b[0]]
-        y = [c[1] for c in b[0]]
-        plot(x,y, mode)
+        P.plot(b.x, b.y, mode)
 
 grid1 = []
 for col in range(10):
     for row in range(10):
-        grid1.append(Polygon([(row,col),(row+1,col),
-                                   (row+1,col+1), (row, col+1),
-                                   (row,col)]))
+        grid1.append(Polygon([row,row+1,row+1,row,row],
+                             [col,col,col+1,col+1,col]))
         
-theta = 3/180.0*pi
+theta = 3/180.0*N.pi
 t_x = 0.04
 t_y = 0.04
 
-tf = array([[cos(theta), -sin(theta), t_x],
-            [sin(theta),  cos(theta), t_y],
-            [0,                0,                1]], 'd')
+tf = N.array([[N.cos(theta), -N.sin(theta), t_x],
+             [N.sin(theta),  N.cos(theta), t_y],
+             [0,             0,            1]], 'd')
 
 # Create grid2 as a rotated version of grid1
 grid2 = []
 for poly in grid1:
-    new_coords = []
-    for point in poly[0]:
-        coord = array([ [point[0]], [point[1]], [1] ], 'd')
-        coord = dot(tf, coord).transpose()
-        new_coords.append(coord[0, 0:2])
+    coords = N.empty((3,len(poly.x)))
+    coords[0] = poly.x
+    coords[1] = poly.y
+    coords[2] = 1.
+    coords = N.dot(tf, coords).transpose()
 
-    grid2.append(Polygon(new_coords))
+    grid2.append(Polygon(coords[:,0], coords[:,1]))
 
 def plot_deformed_grid():
     plotgrid(grid2)
-    axis('equal')
-    xticks([])
-    yticks([])
-    axis([-2,12,-2,12])
-    show()
+    P.axis('equal')
+    P.xticks([])
+    P.yticks([])
+    P.axis([-2,12,-2,12])
+    P.show()
 
-def plot_overlap():
+def plot_overlap(N):
     # Select an overlapping pixel and highlight it
-    N = 54
-    overlap = grid1[N] & grid2[N]
+    p1 = grid1[N]
+    p2 = grid2[N]
+    overlap = zip(*ext.poly_clip(p2.x, p2.y,
+                                 p1.x.min(), p1.x.max(),
+                                 p1.y.max(), p1.y.min()))
     
-    pl1 = pointList(grid1[N] - overlap)
-    pl2 = pointList(grid2[N] - overlap)
-    pl3 = pointList(overlap)
-
-    ax = subplot(111)
-    ax.add_patch(patches.Polygon(pl1, facecolor=[0.9, 0.7, 0.7]))
-    ax.add_patch(patches.Polygon(pl2, facecolor=[0.7, 0.7, 0.9]))
-    ax.add_patch(patches.Polygon(pl3, facecolor=[0.9, 0.8, 0.9]))
+    ax = P.subplot(111)
+    ax.add_patch(patches.Polygon(zip(p1.x,p1.y), facecolor=[0.7, 0.7, 0.9]))
+    ax.add_patch(patches.Polygon(zip(p2.x,p2.y), facecolor=[0.9, 0.8, 0.9]))
+    ax.add_patch(patches.Polygon(overlap, facecolor=[0.3,0.9,0.3]))#facecolor=[0.9, 0.7, 0.7]))    
 
     plotgrid(grid1, 'r-')
     plotgrid(grid2, 'b-')
-    axis('equal')
-    xticks([])
-    yticks([])
-    axis([-2,12,-2,12])
-    show()
+    P.axis('equal')
+    P.xticks([])
+    P.yticks([])
+    P.axis([-2,12,-2,12])
+    P.show()
 
-#plot_deformed_grid()
-plot_overlap()
+plot_overlap(54)
