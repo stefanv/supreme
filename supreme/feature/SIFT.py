b41a65ff64d99583564e24ad6a3c83f9eba40c68
Stefan van der Walt <stefan@voyager>
2006-11-01 18:32:21 +0200

    Feature matching with rejection.

--- supreme/feature/SIFT.py
+++ supreme/feature/SIFT.py
@@ -28,19 +28,29 @@ def fromfile(f):
                 ('data',(float,feature_len))]
     return data.view(datatype)
 
-def match_features(features,featureset):
+def match_features(features,featureset,threshold=0.6):
     """Find the most closely matched feature from a feature-set.
 
     Input:
     ------
     features -- An array of M row-wise features of length N
+
     featureset -- An array of any number of row-wise features of
                   length N.  Typically field 'data' of the record
                   array produced by SIFT.fromfile.
 
     Output:
     -------
-    A list of M indices into featureset.
+    nearest -- An array of indices into featureset.
+
+    distances -- Distances between the provided features and the
+                 closest features in featureset.
+
+    valid -- A boolean array indicating whether the given feature
+             match is valid, according to the criterion described in
+             the SIFT README. It states that a match is valid when the
+             match is less than 0.6 times the distance to the
+             second-closest match.
 
     See original implementation by Tim Hochberg at
 
@@ -49,14 +59,41 @@ def match_features(features,featureset):
     """
     features,featureset = map(N.asarray,[features,featureset])
 
-    nr_features = len(featureset)
-    features_T = features.transpose().copy()
-    totals = N.empty([nr_features,len(features)],float)
-    featureset2 = (featureset**2).sum(-1)
-    featureset2 *= -0.5
-    totals[:] = featureset2[:, N.newaxis]
-    for feat, tot in zip(featureset, totals):
-        for di, ci in zip(features_T, feat):
-            tot += di*ci
+    # We want to find the minimum distance
+    #
+    # N.sqrt((b - a)^2),
+    #
+    # which is the same as finding the minimum distance
+    #
+    # (b - a)^2 or
+    # 1/2 (b - a)^2 which is
+    # b^2/2 - a*b + a^2
+    #
+    # but since a is constant, we minimise over
+    #
+    # b^2/2 - a*b
 
-    return totals.argmax(axis=0)
+    features_T = features.transpose().copy()
+    totals = N.empty([len(featureset),len(features)],float)
+    featureset2 = (featureset**2).sum(-1)
+    featureset2 *= 0.5
+    totals[:] = featureset2[:, N.newaxis]
+    for feat,tot in zip(featureset,totals):
+        for di,ci in zip(features_T,feat):
+            tot -= di*ci
+
+    totals = N.sqrt(totals*2 + (features**2).sum(-1))
+
+    # Pick closest match
+    match = totals.argmin(axis=0)
+    nrange = N.arange(len(features))
+    match_dist = totals[match,nrange]
+
+    # Increase distance to closest match so we can find the
+    # second-closest match
+    totals[match,nrange] = N.inf
+    match2 = totals.argmin(axis=0)
+    match2_dist = totals[match2,nrange]
+    valid_matches = (match_dist <= threshold*match2_dist)
+
+    return match, match_dist, valid_matches
--- supreme/feature/tests/test_SIFT.py
+++ supreme/feature/tests/test_SIFT.py
@@ -38,10 +38,13 @@ class test_SIFT(NumpyTestCase):
 
     def check_match_features(self):
         f = [[0,1,2,3],
-             [3,2,1,2]]
-        featureset = [[3,2,1,0],[0,1,2,3],[9,10,11,12]]
-        assert_equal(feature.SIFT.match_features(f,featureset),
-                     [1,0])
+             [3,2,1,2],
+             [0,1,2,3.1]]
+        featureset = [[3,2,1,0],[0,1,2,3],[9,10,11,12],[3,2,1,0.5]]
+        match,dists,valid = feature.SIFT.match_features(f,featureset)
+        assert_equal(match,[1,3,1])
+        assert_equal(valid,[True,False,True])
+        assert_almost_equal(dists,[0,1.5,0.1])
 
 if __name__ == "__main__":
     NumpyTest().run()
