868931fedaef3dcfcde7833b691309ed4e8fb463
Stefan van der Walt <stefan@sun.ac.za>
2009-10-26 17:29:49 +0200

    Search for an appropriate data directory.

--- supreme/config.py
+++ supreme/config.py
@@ -1,30 +1,23 @@
 """Global configuration and utility functions."""
 
-import numpy as N
-import os.path
-import sys
+import numpy as _np
+import os.path as _osp
+import sys as _sys
 
-__all__ = ['abs_path','add_path','remove_path','ftype','itype',
-           'data_path','lib_path']
+__all__ = ['abs_path', 'ftype', 'itype', 'data_path', 'lib_path']
 
-ftype = N.float64
-itype = N.int32
-eps = N.finfo(ftype).eps
+ftype = _np.float64
+itype = _np.int32
+eps = _np.finfo(ftype).eps
 
 def abs_path(relpath):
     """Given a relative path, return the absolute path."""
-    return os.path.join(os.path.dirname(__file__),
-                        relative_data_path)
+    return _osp.abspath(_osp.join(_osp.dirname(__file__), relpath))
 
-def add_path(absdir):
-    """Add given directory as the first element of the system path."""
-    sys.path.insert(0,absdir)
+data_path = ''
+for dp in ('../data', '../../supreme-data'):
+    abs_dp = abs_path(dp)
+    if _osp.isdir(abs_dp):
+        data_path = abs_dp
 
-def remove_path():
-    """Remove the first element of the system path."""
-    del sys.path[0]
-
-relative_data_path = '../data'
-relative_lib_path = './lib'
-data_path = abs_path(relative_data_path)
-lib_path = abs_path(relative_lib_path)
+lib_path = abs_path('./lib')
