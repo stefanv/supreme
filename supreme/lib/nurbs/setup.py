1387d744b0b83178724edc97a65f72b0588c3032
Stefan van der Walt <stefan@sun.ac.za>
2009-12-05 11:21:24 +0200

    nurbs: Use numpy include dir to build.

--- supreme/lib/nurbs/setup.py
+++ supreme/lib/nurbs/setup.py
@@ -1,9 +1,11 @@
 def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
+    from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
 
     config = Configuration('nurbs', parent_package, top_path)
 
-    config.add_extension('_Bas', ['_Bas.c'])
+    config.add_extension('_Bas', ['_Bas.c'],
+                         include_dirs=[get_numpy_include_dirs()],
+                         )
     config.add_data_dir('doc')
 
     return config
