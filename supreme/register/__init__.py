b6abfad1ffe703c9abb5086976b94937d5a5cb0b
Stefan van der Walt <stefan@sun.ac.za>
2009-12-09 23:09:07 +0200

    Add tests for log polar registration.

--- supreme/register/__init__.py
+++ supreme/register/__init__.py
@@ -7,3 +7,48 @@ from logpolar import patch_match as lp_patch_match
 from logpolar import *
 from correspond import *
 from ncorr import *
+
+def affine_tm(theta=0, tx=0, ty=0, scale=None, scale_x=None, scale_y=None):
+    """Return the transformation matrix for an affine transformation.
+
+    Parameters
+    ----------
+    theta : float
+        Rotation angle in radians.
+    tx, ty : float
+        X and Y translations.
+    scale : float
+        Scaling in both the X and the Y directions.  Defaults to 1.
+    scale_x : float
+        Scaling in the X direction.  Cannot be used together with `scale`.
+    scale_y : float
+        Scaling in the Y direction.  Cannot be used with `scale`.
+
+    Returns
+    -------
+    M : ndarray of float
+        Transformation matrix with the supplied parameters.  Can be used to
+        transform any homogeneous coordinate ``p = [[x, y, 1]].T`` by
+        ``np.dot(M, p)``.
+
+    """
+    if scale is not None:
+        if scale_x is not None or scale_y is not None:
+            raise ValueError("Cannot use scale in combination with "
+                             "`scale_x` or `scale_y`.")
+        scale_x = scale
+        scale_y = scale
+    else:
+        if scale_x is None:
+            scale_x = 1
+        if scale_y is None:
+            scale_y = 1
+
+    a = scale_x
+    b = scale_y
+    C = np.cos(theta)
+    S = np.sin(theta)
+
+    return np.array([[a*C,  -b*S, tx],
+                     [a*S,   b*C, ty],
+                     [0,     0,   1.]])
--- supreme/register/tests/test_logpolar.py
+++ supreme/register/tests/test_logpolar.py
@@ -1,9 +1,25 @@
+import numpy as np
 from numpy.testing import *
+
+import supreme.api as sr
+import scipy.ndimage as ndi
 
 class TestLogpolar:
     def test_basic(self):
-        print "\nTODO: add tests for logpolar.  In the meantime,"
-        print "      run supreme/doc/examples/register_LPT_auto.py"
+        theta = 25 / 180. * np.pi
+        M = sr.register.affine_tm(theta=theta, scale=1.3)
+        shape = np.array([301, 301])
+        M_shift = np.array([[1, 0, -shape[1]/2.],
+                            [0, 1, -shape[0]/2.],
+                            [0, 0,  1]])
+        M = np.dot(np.linalg.inv(M_shift), np.dot(M, M_shift))
+        x = (np.random.random(shape)*255).astype(np.uint8)
+        y = sr.transform.matrix(x, M)
+
+        peak, angle, est_scale = sr.register.lp_patch_match(x, y, angles=360)
+
+        assert_almost_equal(angle, theta, decimal=1)
+        assert_almost_equal(est_scale, 1.3, decimal=1)
 
 if __name__ == "__main__":
     run_module_suite()
