ee379c45a917eb76d08f4434f96b9875863d1d70
Stefan van der Walt <stefan@sun.ac.za>
2009-12-13 21:06:22 +0200

    demo: Make register_mutual_info usable as a script.

--- doc/examples/register_mutual_info.py
+++ doc/examples/register_mutual_info.py
@@ -1,16 +1,22 @@
 import numpy as np
 import scipy.optimize
 import scipy.ndimage
-import matplotlib.pyplot as plt
-
 
 from demo_data import chelsea
 from supreme.register.parzen import joint_hist, mutual_info
 from supreme.transform import homography
 import supreme.register as register
 
-A = chelsea(grey=True).astype(np.uint8)
+import sys
+if len(sys.argv) == 3:
+    from supreme.io import imread
+    A = imread(sys.argv[1], flatten=True).astype(np.uint8)
+    Ac = imread(sys.argv[1], flatten=False).astype(np.uint8)
+    B = imread(sys.argv[2], flatten=True).astype(np.uint8)
+    Bc = imread(sys.argv[2], flatten=False).astype(np.uint8)
 
+
+A = chelsea(grey=True).astype(np.uint8)
 t = 20/180. * np.pi
 x = 3
 y = 6
@@ -34,14 +40,20 @@ def cost(p, A, B):
     S = mutual_info(joint_hist(A, T, win_size=5, std=1))
     return S
 
-
 p = [0, 1, 1, 0, 0],
-for z in range(3, 0, -1):
+for z in range(3, -1, -1):
     print "Downsampling by", 2**z
     A_ = scipy.ndimage.zoom(A, 1/2.**z)
     B_ = scipy.ndimage.zoom(B, 1/2.**z)
     p = scipy.optimize.fmin_powell(cost, p, args=(A_, B_))
-    print p
 
-plt.imshow(register.stack.with_transform([A, B], [np.eye(3), _build_tf(p)]))
-plt.show()
+M = _build_tf(p)
+print "Transformation matrix:"
+print np.array2string(M, separator=', ')
+
+X = register.stack.with_transform([Ac, Bc], [np.eye(3), M], save_tiff=True)
+
+if len(sys.argv) != 3:
+    import matplotlib.pyplot as plt
+    plt.imshow(X)
+    plt.show()
