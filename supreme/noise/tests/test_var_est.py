f2e014787647b23261a6f2ff9906257d54541e87
Stefan van der Walt <stefan@sun.ac.za>
2009-12-19 16:28:21 +0200

    noise: Basic test for windowed variance.

--- supreme/noise/tests/test_var_est.py
+++ supreme/noise/tests/test_var_est.py
@@ -5,8 +5,10 @@ from supreme.noise import *
 
 class TestVarianceEstimate:
     def test_basic(self):
-        x = np.arange(12).astype(np.uint8).reshape((4, 3))
-        print variance(x, 3)
+        x = np.arange(12).astype(np.double).reshape((4, 3))
+        v = variance(x, 3)
+        assert v.shape == (4, 3)
+        assert v[0, 2] == 2.5
 
 if __name__ == "__main__":
     run_module_suite()
