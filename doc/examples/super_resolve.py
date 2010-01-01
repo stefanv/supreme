a5da6bdf6efa66babaa6914fc4f75b849b8759f5
Stefan van der Walt <stefan@sun.ac.za>
2010-01-01 14:00:40 +0200

    examples: super_resolve: Add one frame at a time. Estimate camera standard variance.

--- doc/examples/super_resolve.py
+++ doc/examples/super_resolve.py
@@ -1,14 +1,19 @@
 """Construct super-resolution reconstruction of a registered data-set.
 
 """
-SCALE = 2
+SCALE = 3
+std = None # Auto-detect
+std = 1.25156 # text
+#std = 1.23123012741 # library
 
 import numpy as np
+import scipy.optimize as opt
 
 from supreme.resolve import solve, initial_guess_avg
 from supreme.config import data_path
 from supreme.io import load_vgg
 from supreme.transform import homography
+from supreme.resolve.operators import convolve
 
 import matplotlib.pyplot as plt
 
@@ -22,7 +27,8 @@ else:
 ic = load_vgg(vgg_dir)
 
 images = []
-# Perform photometric registration
+
+# Perform crude photometric registration
 ref = ic[0].copy()
 for i in range(len(ic)):
     img_warp = homography(ic[i], ic[i].info['H'])
@@ -31,31 +37,59 @@ for i in range(len(ic)):
 
     images.append(ic[i] * scale)
 
-##HH = [ic[i].info['H'] for i in range(10)]
-#images = [ic[i] for i in range(10)]
-HH = [i.info['H'] for i in ic]
+HH = [i.info['H'] for i in images]
 oshape = np.floor(np.array(images[0].shape) * SCALE)
 avg = initial_guess_avg(images, HH, SCALE, oshape)
 
-out = solve(images, HH, scale=SCALE, tol=0, std=1.15,
-            x0=avg, damp=7, iter_lim=300)
 
-out = out[50:-10, 10:-50]
-avg = avg[50:-10, 10:-50]
+# Automatically determine best standard deviation parameter
+C = convolve(oshape[0], oshape[1], np.array([[-1, -1, -1],
+                                             [-1,  8, -1],
+                                             [-1, -1, -1]]))
 
-out /= out.max()
-#out = np.clip(out, 0, 255)
-#out = iresolve(images, HH, scale=SCALE,
-#               cost_measure=cost_prior_xsq,
-#               cost_args={'lam': 0.3})
+def std_func(std):
+    out, e = solve(images, HH, scale=SCALE, tol=0, std=std,
+                   x0=avg, damp=6, iter_lim=5, lam=0)
+    out = C * out.flat
+    return np.var(out)
+
+if not std:
+    print "Searching for optimal camera variance..."
+    std = opt.fminbound(std_func, 1.05, 2)
+    print "Std estimated to be:", std
+
+#
+# Solve by adding one frame at a time
+#
+out = avg.copy()
+for j in range(1):
+    print "SR iteration %d" % j
+    for i in range(len(images)):
+        print "Resolving frame %d" % i
+        out = solve([images[i]], [HH[i]], scale=SCALE, tol=0, std=std,
+                    x0=out, damp=6, iter_lim=10, lam=50)
+
+#
+# Solve all at once
+#
+# out = solve(images, HH, scale=SCALE, tol=0, std=1.24,
+#             x0=out, damp=6, iter_lim=40)
+
+
+import scipy.misc
+scipy.misc.imsave('/tmp/avg.png', avg)
+scipy.misc.imsave('/tmp/out.png', out)
+
+out = out[20:-10, 10:-15]
+avg = avg[20:-10, 10:-15]
 
 plt.subplot(3, 1, 1)
 plt.imshow(ic[0], interpolation='nearest', cmap=plt.cm.gray)
 
 plt.subplot(3, 1, 2)
-plt.imshow(avg, interpolation='nearest', cmap=plt.cm.gray)
+plt.imshow(avg, interpolation='lanczos', cmap=plt.cm.gray)
 
 plt.subplot(3, 1, 3)
-plt.imshow(out, interpolation='nearest', cmap=plt.cm.gray)
+plt.imshow(out, interpolation='lanczos', cmap=plt.cm.gray)
 
 plt.show()
