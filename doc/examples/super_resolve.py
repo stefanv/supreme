63a83351991a43148da6357eab917fca6d41644d
Stefan van der Walt <stefan@sun.ac.za>
2010-01-03 03:34:14 +0200

    examples: super_resolve: Better control via configuration keywords.

--- doc/examples/super_resolve.py
+++ doc/examples/super_resolve.py
@@ -1,16 +1,20 @@
 """Construct super-resolution reconstruction of a registered data-set.
 
 """
-SCALE = 2.
+SCALE = 2.5
+UPDATE = False # if False, do a single pass calculation instead of
+               # adding one frame at a time
+DAMP = 1.6
+PHOTOMETRIC_ADJUSTMENT = True
+METHOD = 'LSQR' # 'CG' or 'LSQR' or 'descent'
 
 import numpy as np
-import scipy.optimize as opt
 
 from supreme.resolve import solve, initial_guess_avg
 from supreme.config import data_path
 from supreme.io import load_vgg
 from supreme.transform import homography
-from supreme.resolve.operators import convolve
+from supreme.noise import dwt_denoise
 
 import matplotlib.pyplot as plt
 
@@ -19,53 +23,64 @@ import sys, os
 if len(sys.argv) > 1:
     vgg_dir = sys.argv[1]
 else:
-    vgg_dir = os.path.join(data_path, 'text_small')
+    vgg_dir = os.path.join(data_path, 'vgg/library_very_small')
 
 ic = load_vgg(vgg_dir)
 
-images = []
-
 # Perform crude photometric registration
 ref = ic[0].copy()
-for i in range(len(ic)):
-    img_warp = homography(ic[i], ic[i].info['H'])
-    mask = (img_warp != 0) & (ref != 0)
-    scale = np.mean(ref[mask].astype(float) / img_warp[mask])
+images = []
+scales = []
+for i in range(len(ic) - 20):
+    scale = 1
 
+    if PHOTOMETRIC_ADJUSTMENT:
+        img = ic[i]
+        img_warp = homography(ic[i], ic[i].info['H'])
+        mask = (img_warp > 20) & (img_warp < 220) & \
+               (ref > 20) & (ref < 220)
+        scale = np.mean(ref[mask].astype(float) / img_warp[mask])
+
+    scales.append(scale)
     images.append(ic[i] * scale)
+
+print "Images scaled by: %s" % str(['%.2f' % f for f in scales])
 
 HH = [i.info['H'] for i in images]
 oshape = np.floor(np.array(images[0].shape) * SCALE)
 avg = initial_guess_avg(images, HH, SCALE, oshape)
 
-out = avg.copy()
-
 #
 # Solve by adding one frame at a time
 #
-for j in range(1):
-    print "SR iteration %d" % j
-    for i in range(len(images)):
-        print "Resolving frame %d" % i
-        out = solve([images[i]], [HH[i]], scale=SCALE, tol=0, std=1.05,
-                    x0=out, damp=1e-1, iter_lim=100, lam=1e-8)
 
-#
-# Solve all at once
-#
-#out = solve(images, HH, scale=SCALE, tol=0, std=std,
-#            x0=out, damp=5e-1, iter_lim=200, lam=1e-2, fast=True)
+if UPDATE:
+    #
+    # Update solution one frame at a time
+    #
+    out = avg.copy()
+    for j in range(1):
+        print "SR iteration %d" % j
+        for i in range(len(images)):
+            print "Resolving frame %d" % i
+            import scipy.ndimage as ndi
+            out = ndi.gaussian_filter(out, 0.2)
+            out = solve([images[i]], [HH[i]], scale=SCALE, tol=0,
+                        x0=out, damp=DAMP, iter_lim=200,
+                        method=METHOD)
 
+else:
+    #
+    # Solve all at once
+    #
+    out = avg.copy()
+    out = solve(images, HH, scale=SCALE, tol=0,
+                x0=out, damp=DAMP, iter_lim=200,
+                method=METHOD)
 
 import scipy.misc
 scipy.misc.imsave('/tmp/avg.png', avg)
 scipy.misc.imsave('/tmp/out.png', out)
-
-#out = out[20:-10, 10:-15]
-#avg = avg[20:-10, 10:-15]
-
-#out = out[5:-5, 5:-15]
-#avg = avg[5:-5, 5:-15]
 
 plt.subplot(3, 1, 1)
 plt.imshow(ic[0], interpolation='nearest', cmap=plt.cm.gray)
