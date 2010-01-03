71b587ee6b6c48cfc2e4ecbba31a80bb9815f212
Stefan van der Walt <stefan@sun.ac.za>
2010-01-04 01:03:37 +0200

    examples: super_resolve: Add cmd line options.

--- doc/examples/super_resolve.py
+++ doc/examples/super_resolve.py
@@ -1,13 +1,13 @@
 """Construct super-resolution reconstruction of a registered data-set.
 
 """
-SCALE = 4.
-UPDATE = 1 # if False, do a single pass calculation instead of
-           # adding one frame at a time
-DAMP = 1e-2
-PHOTOMETRIC_ADJUSTMENT = True
-METHOD = 'LSQR' # 'CG' or 'LSQR' or 'descent'
-OPERATOR = 'polygon' # 'polygon' # bilinear or polygon
+## SCALE = 4
+## UPDATE = 1 # if False, do a single pass calculation instead of
+##            # adding one frame at a time
+## DAMP = 1e-8
+## PHOTOMETRIC_ADJUSTMENT = True
+## METHOD = 'LSQR' # 'CG' or 'LSQR' or 'descent'
+## OPERATOR = 'polygon' # 'polygon' # bilinear or polygon
 
 import numpy as np
 
@@ -20,11 +20,42 @@ from supreme.noise import dwt_denoise
 import matplotlib.pyplot as plt
 
 import sys, os
+from optparse import OptionParser
+
+usage = "%prog [options] vgg_dir"
+parser = OptionParser(usage=usage)
+parser.add_option('-s', '--scale',
+                  help='Resolution improvement required [default: %default]')
+parser.add_option('-d', '--damp',
+                  help='Damping coefficient -- '
+                       'suppresses oscillations [default: %default]')
+parser.add_option('-m', '--method', dest='method',
+                  help='`CG`, `LSQR` or `descent`. '
+                  'Specifies optimisation algorithm [default: %default]')
+parser.add_option('-o', '--operator', dest='operator',
+                  help='`polygon` or `bilinear`. The camera '
+                       'model is approximated by this '
+                       'interpolation scheme. [default: %default]')
+parser.add_option('-u', '--update', action='store_true',
+                  help='Use images as incremental evidence [default: %default]')
+parser.add_option('-p', '--photo-adjust',
+                  action='store_false',
+                  help='Perform photometric adjustment [default: %default]')
+
+parser.set_defaults(scale=2,
+                    damp=1e-1,
+                    method='CG',
+                    operator='polygon',
+                    update=False,
+                    photo_adjust=True)
+
+(options, args) = parser.parse_args()
 
 if len(sys.argv) > 1:
-    vgg_dir = sys.argv[1]
+    vgg_dir = sys.argv[-1]
 else:
-    vgg_dir = os.path.join(data_path, 'vgg/library_very_small')
+    parser.print_help()
+    sys.exit(0)
 
 ic = load_vgg(vgg_dir)
 
@@ -35,7 +66,7 @@ scales = []
 for i in range(len(ic)):
     scale = 1
 
-    if PHOTOMETRIC_ADJUSTMENT:
+    if options.photo_adjust:
         img = ic[i]
         img_warp = homography(ic[i], ic[i].info['H'])
         mask = (img_warp > 20) & (img_warp < 220) & \
@@ -48,14 +79,14 @@ for i in range(len(ic)):
 print "Images scaled by: %s" % str(['%.2f' % f for f in scales])
 
 HH = [i.info['H'] for i in images]
-oshape = np.floor(np.array(images[0].shape) * SCALE)
-avg = initial_guess_avg(images, HH, SCALE, oshape)
+oshape = np.floor(np.array(images[0].shape) * options.scale)
+avg = initial_guess_avg(images, HH, options.scale, oshape)
 
 #
 # Solve by adding one frame at a time
 #
 
-if UPDATE:
+if options.update:
     #
     # Update solution one frame at a time
     #
@@ -64,18 +95,18 @@ if UPDATE:
         print "SR iteration %d" % j
         for i in range(len(images)):
             print "Resolving frame %d" % i
-            out = solve([images[i]], [HH[i]], scale=SCALE, tol=0,
-                        x0=out, damp=DAMP, iter_lim=200,
-                        method=METHOD, operator=OPERATOR)
+            out = solve([images[i]], [HH[i]], scale=options.scale, tol=0,
+                        x0=out, damp=options.damp, iter_lim=200,
+                        method=options.method, operator=options.operator)
 
 else:
     #
     # Solve all at once
     #
     out = avg.copy()
-    out = solve(images, HH, scale=SCALE, tol=0,
-                x0=out, damp=DAMP, iter_lim=200,
-                method=METHOD)
+    out = solve(images, HH, scale=options.scale, tol=0,
+                x0=out, damp=options.damp, iter_lim=200,
+                method=options.method)
 
 import scipy.misc
 scipy.misc.imsave('/tmp/avg.png', avg)
