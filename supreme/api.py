469ce44c028d02e57bcab5519059f3f41384dd21
Stefan van der Walt <stefan@sun.ac.za>
2010-01-06 01:48:17 +0200

    Add test data and image display functions to simplify demonstration code.

--- supreme/api.py
+++ supreme/api.py
@@ -18,3 +18,30 @@ def iterable(x):
         return False
     else:
         return True
+
+def test_data():
+    """Return an image for testing purposes.
+
+    Returns
+    -------
+    I : ndarray of uint8
+        512x512 test image.
+
+    """
+    import os
+    f = os.path.join(os.path.dirname(__file__),
+                     'lib/pywt/demo/data/aero.png')
+    return io.imread(f)
+
+def show(*images):
+    """Display images on screen.
+
+    """
+    import matplotlib.pyplot as plt
+
+    L = len(images)
+    for i in range(L):
+        plt.subplot(1, L, i + 1)
+        plt.imshow(images[i], cmap=plt.cm.gray, interpolation='nearest')
+
+    plt.show()
