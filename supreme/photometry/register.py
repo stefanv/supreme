91775117709b7dcc87868bf4054feabafd1dfae8
Stefan van der Walt <stefan@sun.ac.za>
2010-01-15 14:33:49 +0200

    Photometry: Add histogram matching.

--- supreme/photometry/register.py
+++ supreme/photometry/register.py
@@ -15,7 +15,7 @@ def photometric_adjust(source, target):
         target.
 
     """
-    mask = (source > 0) & (target > 0) & (source < 210) & (target < 210)
+    mask = (source > 10) & (target > 10) & (source < 210) & (target < 210)
 
     mu_s = np.mean(source[mask])
     std_s = np.std(source[mask])
@@ -26,3 +26,43 @@ def photometric_adjust(source, target):
     b = (mu_t * std_s - mu_s * std_t)/std_s
 
     return a, b
+
+def histogram_adjust(source, target):
+    """Transform the histogram of the source so that it is similar
+    to that of the target.
+
+    Parameters
+    ----------
+    source, target = ndarray
+        Source and target images.
+
+    Returns
+    -------
+    source_adj : ndarray
+        Source, adjusted so that its histogram looks like that of
+        target.
+
+    """
+    s_hist, s_edges = np.histogram(source, bins=256)
+    t_hist, t_edges = np.histogram(target, bins=256)
+
+    s_edges = s_edges[:-1]
+    t_edges = t_edges[:-1]
+
+    s_cum = np.cumsum(s_hist)
+    t_cum = np.cumsum(t_hist)
+
+    mapping = np.argmin(np.abs(t_cum - s_cum[:, None]), axis=1)
+
+    sshape = source.shape
+    source = source.flat
+    source = source/np.max(source) * (len(s_edges) - 1)
+    source = np.clip(np.round(source), 0, len(s_edges) - 1).astype(int)
+
+#    import matplotlib.pyplot as plt
+#    plt.plot(mapping)
+#    plt.show()
+
+    return t_edges[mapping[source]].reshape(sshape) + \
+           (t_edges[1] - t_edges[0])/2.
+
--- supreme/photometry/tests/test_register.py
+++ supreme/photometry/tests/test_register.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.testing import *
-from supreme.photometry import photometric_adjust
+from supreme.photometry import photometric_adjust, histogram_adjust
 
 def test_basic():
     x = np.random.random((100, 100)) * 255
@@ -10,6 +10,14 @@ def test_basic():
 
     assert_almost_equal(a, 1.8)
     assert_almost_equal(b, 0)
+
+def test_histogram():
+    x = np.random.random((100, 100)) * 200
+    y = (x/x.max())**0.8
+
+    z = histogram_adjust(y, x)
+
+    assert np.mean((z - x)**2) < 1
 
 if __name__ == "__main__":
     run_module_suite()
